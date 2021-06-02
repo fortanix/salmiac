ae618bc91c172f4cc52ea980c914a8146a7b869f
NikitaShyrei <nikita.shyrei@fortanix.com>
2021-06-02 13:13:43 +0200

    - initial vsock-proxy commit. Vsock-proxy transfers traffic from remote to nitro enclave inside EC2

---
+++ vsock-proxy/src/lib.rs
@@ -0,0 +1,126 @@
+use log::info;
+use nix::sys::socket::{SockAddr};
+use std::io::{Read, Write};
+use std::net::{IpAddr, SocketAddr, TcpStream, TcpListener};
+use vsock::{VsockListener, VsockStream};
+
+pub const VSOCK_PROXY_CID: u32 = 3;
+
+pub struct Proxy {
+    local_port: u32,
+    remote_addr: IpAddr,
+    remote_port: u16,
+}
+
+impl Proxy {
+    pub fn new_simple(local_port: u32, remote_addr: IpAddr, remote_port: u16) -> Self {
+        Proxy {
+            local_port,
+            remote_addr,
+            remote_port,
+        }
+    }
+
+    pub fn listen_enclave(&self) -> Result<VsockListener, String> {
+        let sockaddr = SockAddr::new_vsock(VSOCK_PROXY_CID, self.local_port);
+        let listener = VsockListener::bind(&sockaddr)
+            .map_err(|_| format!("Could not bind to {:?}", sockaddr))?;
+        info!("Bound to {:?}", sockaddr);
+
+        Ok(listener)
+    }
+
+    pub fn connect_enclave(&self, cid: u32) -> Result<VsockStream, String> {
+        let sockaddr = SockAddr::new_vsock(cid, self.local_port);
+        let connected = VsockStream::connect(&sockaddr)
+            .map_err(|err| format!("Failed to connect to enclave: {:?}", err));
+
+        connected
+    }
+
+    pub fn listen_remote(&self) -> Result<TcpListener, String> {
+        let sock_addr = SocketAddr::new(self.remote_addr, self.remote_port);
+
+        TcpListener::bind(sock_addr).map_err(|err| format!("Failed to listen to external port {:?}", err))
+    }
+
+    pub fn connect_remote(&self) -> Result<TcpStream, String> {
+        let sock_addr = SocketAddr::new(self.remote_addr, self.remote_port);
+
+        TcpStream::connect(sock_addr).map_err(|err| format!("Failed to connect to external port {:?}", err))
+    }
+}
+
+pub fn transfer_to_enclave(vsock : &mut VsockStream, listener : &mut TcpListener) -> Result<(), String> {
+    loop {
+        let (mut incoming, _) = listener.accept().map_err(|err| format!("Accept failed: {:?}", err))?;
+
+        let received = receive_string(&mut incoming);
+
+        let r = received.and_then(|r| {
+            println!("{}", r);
+            send_string(vsock, r)
+        });
+
+        return r;
+    }
+}
+
+fn receive_string(tcp :&mut TcpStream) -> Result<String, String> {
+    let len = receive_u64(tcp)?;
+    let mut buf = [0u8; 8192];
+    receive_bytes(tcp, &mut buf, len)?;
+
+    let received = String::from_utf8(buf.to_vec()).map_err(|err| format!("The received bytes are not UTF-8: {:?}", err));
+
+    received
+}
+
+fn receive_bytes(tcp: &mut TcpStream, buf: &mut [u8], len: u64) -> Result<(), String> {
+    let len = len as usize;
+    let mut recv_bytes = 0;
+
+    while recv_bytes < len {
+        let size = match tcp.read(&mut buf[recv_bytes..len]) {
+            Ok(size) => size,
+            //Err(std::io::Error(_)) => 0,
+            Err(err) => return Err(format!("{:?}", err)),
+        };
+        recv_bytes += size;
+    }
+
+    Ok(())
+}
+
+fn receive_u64(tcp: &mut TcpStream) -> Result<u64, String> {
+    use std::mem::size_of;
+    use byteorder::LittleEndian;
+    use byteorder::ByteOrder;
+
+    let mut buf = [0u8; size_of::<u64>()];
+    let size = size_of::<u64>() as u64;
+
+    receive_bytes(tcp, &mut buf, size).map(|_e| LittleEndian::read_u64(&buf))
+}
+
+fn send_u64(tcp: &mut dyn Write, val: u64) -> Result<(), String> {
+    use std::mem::size_of;
+    use byteorder::LittleEndian;
+    use byteorder::ByteOrder;
+
+    let mut buf = [0u8; size_of::<u64>()];
+    LittleEndian::write_u64(&mut buf, val);
+
+    send_bytes(tcp, &mut buf)
+}
+
+pub fn send_string(tcp: &mut dyn Write, data : String) -> Result<(), String> {
+    let buf = data.as_bytes();
+    let len = buf.len() as u64;
+
+    send_u64(tcp, len).and_then(|_e| send_bytes(tcp, &buf))
+}
+
+fn send_bytes(tcp: &mut dyn Write, buf: &[u8]) -> Result<(), String> {
+    tcp.write_all(buf).map_err(|err| format!("Failed to write bytes to external socket {:?}", err))
+}
---
+++ vsock-proxy/src/main.rs
@@ -0,0 +1,119 @@
+use clap::{App, AppSettings, Arg, SubCommand, ArgMatches};
+
+use env_logger;
+use log::info;
+//use std::env;
+
+use vsock_proxy::{
+    Proxy,
+    transfer_to_enclave,
+    send_string
+};
+
+use std::net::{IpAddr, Ipv4Addr};
+use std::process::exit;
+
+fn main() -> Result<(), String> {
+    env_logger::init();
+
+    let matches = console_arguments();
+
+    match matches.subcommand() {
+        ("proxy", Some(args)) => {
+            let local_port = args
+                .value_of("vsock-port")
+                .map(|e| e.parse::<u32>())
+                .expect("vsock-port must be specified")
+                .map_err(|err| format!("Cannot parse int {:?}", err))?;
+
+            let remote_port = args
+                .value_of("remote-port")
+                .map(|e| e.parse::<u16>())
+                .expect("remote-port must be specified")
+                .map_err(|err| format!("Cannot parse int {:?}", err))?;
+
+            let cid = args
+                .value_of("cid")
+                .map(|e| e.parse::<u32>())
+                .expect("cid must be specified")
+                .map_err(|err| format!("Cannot parse int {:?}", err))?;
+
+            let proxy = Proxy::new_simple(local_port, IpAddr::V4(Ipv4Addr::UNSPECIFIED), remote_port);
+
+            let mut vsock_stream = proxy.connect_enclave(cid)?;
+
+            info!("Connected to enclave!");
+
+            let mut remote_listener = proxy.listen_remote()?;
+
+            info!("Listening to external socket!");
+
+            transfer_to_enclave(&mut vsock_stream, &mut remote_listener)?;
+        }
+        ("client", Some(args)) => {
+            let remote_port = args
+                .value_of("remote-port")
+                .map(|e| e.parse::<u16>())
+                .expect("remote-port must be specified")
+                .map_err(|err| format!("Cannot parse int {:?}", err))?;
+
+            let client = Proxy::new_simple(0, IpAddr::V4(Ipv4Addr::new(63,35,162,90)), remote_port);
+
+            let mut ec2_connect = client.connect_remote()?;
+
+            info!("Connected to EC2!");
+
+            let data = "Hello, world from client!".to_string();
+            send_string(&mut ec2_connect, data)?;
+        }
+        (_, _) => {
+            info!("Program mus be either 'proxy' or 'client'");
+            exit(1);
+        }
+    }
+
+    exit(0);
+}
+
+fn console_arguments<'a>() -> ArgMatches<'a> {
+    App::new("Vsock proxy")
+        .about("Vsock proxy")
+        .setting(AppSettings::DisableVersion)
+        .subcommand(
+            SubCommand::with_name("proxy")
+                .about("Transfer traffic from/to enclave")
+                .arg(
+                    Arg::with_name("remote-port")
+                        .long("remote-port")
+                        .help("remote port")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("cid")
+                        .long("cid")
+                        .help("cid")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("vsock-port")
+                        .long("vsock-port")
+                        .help("vsock port")
+                        .takes_value(true)
+                        .required(true),
+                )
+        )
+        .subcommand(
+            SubCommand::with_name("client")
+                .about("Connect to EC2")
+                .arg(
+                    Arg::with_name("remote-port")
+                        .long("remote-port")
+                        .help("remote port")
+                        .takes_value(true)
+                        .required(true),
+                )
+        )
+        .get_matches()
+}
