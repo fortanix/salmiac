529456069096e45d7d8a3830534d7af1afe51248
vinicius.artur <vinicius.artur@fortanix.com>
2021-07-02 16:32:46 +0200

    test4

--- pipeline.sh
+++ pipeline.sh
@@ -2,4 +2,24 @@
 
 set -Eeuox pipefail
 
+source devops/tools/bitbucket-api/bitbucket-buildstatus.sh
+
+testname="salmiac-pr"
+description="this is the description of the test"
+link="https://jenkins.fortanix.net/job/devops-pr/${BUILD_NUMBER}/"
+
+updateStatus () {
+    result=$1
+    echo "Updating status on commit ${commit_hash} to ${result}"
+    BitBucketGetAccessToken "${bitbucket_key}" "${bitbucket_secret}"
+	BitBucketSetStatus "${result}" ${repo_name} ${commit_hash} ${TOKEN} ${testname} ${link} "${description}"
+}
+
+trap "updateStatus FAILED" EXIT
+
+updateStatus "INPROGRESS"
+
 ./ci.sh
+
+updateStatus "SUCCESSFUL"
+trap "" EXIT
