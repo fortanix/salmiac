03a434f9ffad548449b3230be596d648846e6490
NikitaShyrei <nikita.shyrei@fortanix.com>
2022-11-01 14:47:38 +0100

    - comments

--- enclave-startup/src/main.rs
+++ enclave-startup/src/main.rs
@@ -2,17 +2,14 @@ use std::env;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 
-/// A program that implements a working directory switch before running user application.
-/// Working directory different from root ("/") comes from a client images with WORKDIR clause
-/// (https://docs.docker.com/engine/reference/builder/#workdir).
-/// Because WORKDIR is not currently supported by a Nitro converter (https://github.com/aws/aws-nitro-enclaves-cli/issues/388)
-/// we have to perform directory switch manually to prevent any sort of file not found errors from happening.
+/// A program that switches working directory, user and group before running the application.
+/// Working directory and user/group come from a client images with following clauses:
+/// (https://docs.docker.com/engine/reference/builder/#workdir),
+/// (https://docs.docker.com/engine/reference/builder/#user).
+/// Because neither WORKDIR or USER are currently supported by a Nitro converter
+/// we have to perform switch manually to prevent any sort of access and file not found errors from happening.
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() < 3 {
-        return Err("Working directory (first argument) and binary to run (second argument) must be supplied.".to_string());
-    }
 
     let workdir = &args[1];
     let user = &args[2];
@@ -23,6 +20,8 @@ fn main() -> Result<(), String> {
 
     env::set_current_dir(workdir).map_err(|err| format!("Failed to set work dir to {}. {:?}", workdir, err))?;
 
+    // We use `runuser` only for the user different from `root`,
+    // because `root` is a default user inside an enclave.
     let mut client_command = if user != "root" {
         let mut result = Command::new("runuser");
 
