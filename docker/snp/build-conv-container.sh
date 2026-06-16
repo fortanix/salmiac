25e9b051bf9fda9e087635a173c4edfc0d1233a4
Naveen Kumar G N <naveen.kumar@fortanix.com>
2026-06-16 14:09:55 +0530

    [RTE-1045] parent-base-image changes to support tdx conversion (#95)
    - Remove redundant dnsmasq.d by passing --build-context to "docker build"
    - Move parent-base Dockerfile to qemu directory as TDX CVMs also uses the same
    - Move enclave-base-gpu from docker/snp to docker/

--- docker/snp/build-conv-container.sh
+++ docker/snp/build-conv-container.sh
@@ -11,7 +11,7 @@ mv ./staging/container-converter ./staging/server
 
 # Package the enclave and parent base images for the converter
 docker save -o ./staging/enclave-base.tar enclave-base
-docker save -o ./staging/enclave-base-gpu.tar enclave-base-snp-gpu
+docker save -o ./staging/enclave-base-gpu.tar enclave-base-gpu
 docker save -o ./staging/parent-base.tar parent-base-snp
 
 # Stage sev-snp-measure tooling for the converter image
