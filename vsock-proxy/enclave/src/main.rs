dc0d4f66c4f2e5e80da8ed66c5208f893b86fe3f
aditijannu <aditi.jannu@fortanix.com>
2026-09-25 14:13:17 +0200

    Configure fetching datasets from CCM only on nitro platform

--- vsock-proxy/enclave/src/main.rs
+++ vsock-proxy/enclave/src/main.rs
@@ -3,12 +3,13 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+#[cfg(platform = "nitro")]
 mod app_configuration;
 mod certificate;
 mod enclave;
 mod file_system;
 mod platform;
+#[cfg(platform = "nitro")]
 mod utils;
 
 use std::path::Path;
